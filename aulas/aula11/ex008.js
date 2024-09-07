557f64529ca1998810a2ea950cf79b4f251e2bf3
Cristhian Esteves <132605397+Cristhianstevs@users.noreply.github.com>
2024-09-07 14:13:47 -0300

    Initial commit

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`A velocidade do seu carro é de ${vel}km/s`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança`)
