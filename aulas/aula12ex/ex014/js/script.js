1e80d9b12c15b86ecdd1fe433e1ee6b9c37a46fd
Cristhian Esteves <132605397+Cristhianstevs@users.noreply.github.com>
2024-09-12 21:53:30 -0300

    Ataualizações javascript

--- aulas/aula12ex/ex014/js/script.js
+++ aulas/aula12ex/ex014/js/script.js
@@ -3,18 +3,20 @@ function carregar(){
     var img = document.getElementById('img')
     var data = new Date()
     var hora = data.getHours()
-    // var hora = 22
+    // var hora = 0
     
     if (hora >= 6 && hora < 12) {
         var acenar = 'Bom dia!'
         img.src = 'img/montanha_manha_pq.png'
+            msg.innerHTML = `<p> ${acenar} Agora são ${hora} horas. </p>`
     }
     else if (hora >= 12 && hora < 19) {
         var acenar = 'Boa tarde!'
         img.src = 'img/montanha_tarde_pq.png'
         document.body.style.backgroundColor = '#a66943'
+            msg.innerHTML = `<p> ${acenar} Agora são ${hora} horas. </p>`
     }
-    else if (hora > 24) {
+    else if (hora >= 24) {
         msg.innerHTML = `<p> ${hora} horas não existe </p>`
         img.src = 'img/gato_meme_err.png'
         document.body.style.backgroundColor = '#909090'
@@ -23,8 +25,7 @@ function carregar(){
         var acenar = 'Boa noite!'
         img.src = 'img/montanha_noite_pq.png'
         document.body.style.backgroundColor = '#252a2b'
+            msg.innerHTML = `<p> ${acenar} Agora são ${hora} horas. </p>`
     }
-    
-    msg.innerHTML = `<p> ${acenar} Agora são ${hora} horas. </p>`
 
 }
