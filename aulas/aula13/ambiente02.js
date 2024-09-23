61d72692d85e0a07a023710a1950ea718ced58d8
Cristhian Esteves <c.gasparesteves@gmail.com>
2024-09-23 19:52:04 -0300

    Atualizações javascript

---
+++ aulas/aula13/ambiente02.js
@@ -0,0 +1,18 @@
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+while (contador <= 6)
+
+
+
+/*
+console.log(`Passo 1`)
+console.log(`Passo 2`)
+console.log(`Passo 3`)
+console.log(`Passo 4`)
+console.log(`Passo 5`)
+console.log(`Passo 6`)
+*/
---
+++ aulas/aula13/comerPizza.js
@@ -0,0 +1,44 @@
+var pizza = 7 // Número de Pizzas
+var fatias = 4 // Número de Fatias
+var comerFatia = pizza * fatias // Quantas fatias tem no total
+
+while (comerFatia > 0) { // Enquanto comerFatia for maior que zero, faça
+    console.log(`Comi ${comerFatia} pedaços`) 
+    comerFatia--
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var comerFatia = 8
+
+while (comerFatia <= 8  && comerFatia >= 1) {
+    console.log(`Comi a ${comerFatia} fatia`)
+    comerFatia--
+}
+*/
