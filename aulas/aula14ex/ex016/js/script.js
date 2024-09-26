eb1738c8f6f0efab1c7e9792ac5d3741914fdccd
Cristhian Esteves <c.gasparesteves@gmail.com>
2024-09-26 18:42:51 -0300

    Atualizações javascript

--- aulas/aula14ex/ex016/js/script.js
+++ aulas/aula14ex/ex016/js/script.js
@@ -1,6 +1,7 @@
 function contar() {
     
     var resultado = document.querySelector('div.resultado')
+    resultado.innerHTML = ``
     
     function getNumber(selector) { 
         return Number(document.querySelector(selector).value)
@@ -14,22 +15,33 @@ function contar() {
     if (isNaN(numeroInicio) || numeroInicio === 0){
         window.alert('Digite um número de início.')
     }
-    else if (numeroInicio > numeroFinal) {
-        window.alert('O número de início não pode ser maior que o número final.')
-    }
-    else if (numeroFinal < numeroPulo) {
-        window.alert('O numero de passo não pode ser maior que o número final.')
-    }
     else if (numeroPulo <= 0) {
         window.alert('O passo deve ser maior que zero.');
     }
     else {
         
         resultado.innerHTML += `Vamos contar de ${numeroInicio} até ${numeroFinal} pulando de ${numeroPulo} em ${numeroPulo}<br>`
-        while (numeroInicio <= numeroFinal) {
-            resultado.innerHTML += `${numeroInicio}, `
-            numeroInicio += numeroPulo
+        var contagem = 0
+
+        if (numeroInicio > numeroFinal) {
+            
+            for (var contagem = numeroInicio; contagem >= numeroFinal; contagem -= numeroPulo) {
+                resultado.innerHTML += `${contagem}, `
+            }
+
         }
-        resultado.innerHTML += '<br><br>'
+        else { 
+
+            for (var contagem = numeroInicio; contagem <= numeroFinal; contagem += numeroPulo) {
+                resultado.innerHTML += `${contagem}, `
+            }
+
+            /*
+            while (numeroInicio <= numeroFinal) {
+                resultado.innerHTML += `${numeroInicio}, `
+                numeroInicio += numeroPulo
+            }
+            */                                                   
+        }
     }
 }
--- aulas/aula14ex/ex017/js/script.js
+++ aulas/aula14ex/ex017/js/script.js
@@ -1,12 +1,12 @@
 function gerarTabuada() {
     
-    var resultado = document.querySelector('p.resultado')
-    resultado.innerHTML = ``
+    var tabuada = document.querySelector('select#seltab')
+    tabuada.innerHTML = ``
     
     function getNumber(selector) {
         return Number(document.querySelector(selector).value)
     }
-
+    
     var numeroTabuada = getNumber('#numero_tabuada') // Selector recebe #numero_tabuada e converte para number.
     
     if (isNaN(numeroTabuada) || numeroTabuada === 0) {
@@ -15,10 +15,13 @@ function gerarTabuada() {
     else {
         var multiplicador = 1
         var resultadoMultiplicação = 0
-
+        
         while (resultadoMultiplicação < numeroTabuada * 10) {
+            let item = document.createElement('option')
+            item.text = `${numeroTabuada} x ${multiplicador} = ${resultadoMultiplicação}`
+            item.value = `tabuada${multiplicador}` // Essa linha não faz sentido no JavaScript mas faria em uma linguagem de programação Back-end
+            tabuada.appendChild(item)
             resultadoMultiplicação = numeroTabuada * multiplicador
-            resultado.innerHTML += `${numeroTabuada} x ${multiplicador} = ${resultadoMultiplicação} <br>`
             multiplicador ++
         }
     }
