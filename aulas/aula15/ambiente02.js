ed57e1da933802540cbf7a372d15de4c6292b5e0
Cristhian Esteves <c.gasparesteves@gmail.com>
2024-09-29 15:40:27 -0300

    Atualizações javascript

--- aulas/aula15/ambiente02.js
+++ aulas/aula15/ambiente02.js
@@ -1,8 +1,11 @@
 let num = ['lambo', 'porche', 'ferrari', 'lotus']
+//  num = [   0   ,    1    ,     2     ,   3   ]
 
-num.sort()
+
+num.sort() // Usamos para organizar um vetor, alfabetica caso string e crescente caso number.
+
+let positionVetor = num.indexOf('ferrari') // Achar 
 
 
-let positionVetor = num.indexOf(3)
-console.log(`O valor 3 está na posição ${positionVetor}`)
-console.log(`Nosso vetor é ${num}`)
+console.log(`Usando 'num.sort()' nosso vetor foi reorganizado para: ${num}`)
+console.log(`Udando "num.indexOf('ferrari')" o valor 'ferrari' está na posição ${positionVetor}`)
--- aulas/aula15/vetoresNaTela.js
+++ aulas/aula15/vetoresNaTela.js
@@ -1,14 +1,15 @@
 let vetores = [3, 5, 2, 6, 7, 1, 4]
+//  vetores = [0, 1, 2, 3, 4, 5, 6] // chaves ou indices
 
 /* 
 
-console.log(votores[0])
-console.log(votores[1])
-console.log(votores[2])
-console.log(votores[3])
-console.log(votores[4])
-console.log(votores[5])
-console.log(votores[6])
+console.log(votores[0]) == 3
+console.log(votores[1]) == 5
+console.log(votores[2]) == 2
+console.log(votores[3]) == 6
+console.log(votores[4]) == 7
+console.log(votores[5]) == 1
+console.log(votores[6]) == 4
 
 
 for (positionVetor = 0; positionVetor < vetores.length; positionVetor++) {
@@ -16,8 +17,6 @@ for (positionVetor = 0; positionVetor < vetores.length; positionVetor++) {
 }
 
 */
-
-console.log(``)
 
 for (positionVetor in vetores) {
     console.log(`A posição ${positionVetor} tem o valor ${vetores[positionVetor]}`)
